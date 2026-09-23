907d2ca68e24443af9fb8a8b1a394f92754925ed
BijettBridgette <bridgettemel8@gmail.com>
2026-09-23 12:18:18 +0800

    cmc

--- labs/lab08/exercise4/exercise4.py
+++ labs/lab08/exercise4/exercise4.py
@@ -1,7 +1,15 @@
 current_reading = int(input())
 previous_reading = int(input())
 
+consumption = current_reading - previous_reading
 
+if consumption <= 20 :
+    water_cost = 0.57 * consumption
+elif consumption <= 35 : 
+    water_cost = (1.03 * (consumption-20)) + (0.57 * 20)
+else :
+    water_cost = (1.40 * (consumption - 35)) + (1.03 * 15) + (0.57 * 20)
+total_bill = water_cost  + 8 + 2
 
 print(consumption)
 print(water_cost)
